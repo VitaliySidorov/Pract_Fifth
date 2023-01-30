fd918cb39b9092906f1f023d01241f69c76653db
Vitaliy <vitaliysidorov@yandex.ru>
2023-01-30 21:17:11 +0500

    add Solution if Task 034

---
+++ 034_Task/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int num = ReadNumber();
+Console.Write("Исходный массив ");
+int[] array = FillArray(num);
+PrintArray(array);
+
+Console.WriteLine("Количество четных чисел в массиве " + CountEven(array));
+
+int ReadNumber() // Метод проверки соответствия вводимого числа условиям задачи
+{
+    while (true)
+    {
+        Console.Write("Введите количество элементов массива: ");
+        if (int.TryParse(Console.ReadLine(), out int number) && number > 0)
+        {
+            return number;
+        }
+        else Console.WriteLine("Не удалось распознать требуемое число, попробуйте еще раз.");
+    }
+}
+int[] FillArray(int n) // Метод заполнения массива случайными числами
+{
+    int[] array = new int[n];
+    Random number = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = number.Next(100, 1000); // [100, 1000)
+    }
+    return array;
+}
+int CountEven(int[] arr) // Метод поиска четных чисел
+{
+    int countEven = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            countEven++;
+        }
+    }
+    return countEven;
+}
+void PrintArray(int[] array) // Метод вывода массива в консоль
+{
+    Console.WriteLine($"[{String.Join(", ", array)}]");
+}
